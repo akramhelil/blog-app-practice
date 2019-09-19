f430dad8b2497f2c413a0ddcc1776461bf7be68e
akramhelil <akramhelil@gmail.com>
2019-09-19 18:55:17 -0400

    added view files setup the first route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 // APP mongoose CONFIG
-mongoose.connect("mongodb://localhost/blog-app", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/blog-app", { useNewUrlParser: true, useUnifiedTopology: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +19,33 @@ const blogSchema = new mongoose.Schema({
         { type: Date, default: Date.now }
 });
 
-// Creating the Model 
 const Blog = mongoose.model('Blog', blogSchema);
+
+Blog.create({
+    title: 'Test Blog',
+    image: 'https://images.unsplash.com/photo-1568893472376-821efb43f1ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    body: 'CREATED the BLOG',
+
+})
+
+// RESTFUL ROUTES
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log("Theres, ERROOOOOR")
+        } else {
+            res.render('index', {blogs: blogs})
+        }
+    })
+
+})
+
+
+
 
 
 app.listen(PORT, () => {
